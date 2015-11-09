c0a0241d956f3e5bb2b6a3e2bf377f4736a839bc
Michelle Griebel <mgriebel@enventis.net>
2015-11-09 22:37:07 +0000

    Validations Added

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+      validates :first_name, :last_name, :email, presence: true
+      has_many :pets, dependent: :destroy
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ActiveRecord::Base
+    validates :name, :variety, :person_id, presence: true
+    belongs_to :person
+    VARIETIES=%w(dog cat fish bird)
+    validates :variety, inclusion:{in:Pet::VARIETIES}
 end
